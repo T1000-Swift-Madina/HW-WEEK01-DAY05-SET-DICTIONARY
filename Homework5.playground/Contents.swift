6ec57046781ca9d04b09f0855843741ce8f41a4f
RahafAbdulaal <Rahaf.Abdulaal.med.s@tuwaiq.edu.sa>
2021-10-01 01:47:54 +0300

    Homework2

---
+++ Homework5.playground/Contents.swift
@@ -0,0 +1,37 @@
+import Cocoa
+
+var Ahmed : Set<String> = []
+var sulyman : Set<String> = []
+
+Ahmed.insert("Jeddah")
+Ahmed.insert("Makkah")
+Ahmed.insert("Yanbu")
+sulyman.insert("Jeddah")
+sulyman.insert("Makkah")
+sulyman.insert("Tabuk")
+sulyman.insert("Khobar")
+sulyman.insert("Hail")
+sulyman.insert("Jezan")
+
+var visitedCities = Ahmed.union(sulyman)
+print(visitedCities)
+var AhmedVisitedCity = [String: Int]()
+var SulymanVisitedCity = [String: Int]()
+
+AhmedVisitedCity["Jeddah"] = 6
+AhmedVisitedCity["Makkah"] = 9
+AhmedVisitedCity["Yanbu"] = 1
+
+SulymanVisitedCity["Jeddah"] = 6
+SulymanVisitedCity["Makkah"] = 9
+SulymanVisitedCity["Tabuk"] = 5
+SulymanVisitedCity["Khobar"] = 2
+SulymanVisitedCity["Hail"] = 70
+SulymanVisitedCity["Jezan"] = 8
+
+print(AhmedVisitedCity)
+print(SulymanVisitedCity)
+
+
+
+
