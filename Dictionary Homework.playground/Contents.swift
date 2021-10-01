4d72d3c34a76a9bdef1972d86606a6a14e69dffc
NUJUD3 <nojoudmohaimmed@hotmail.com>
2021-10-02 01:50:45 +0300

    add

---
+++ Dictionary Homework.playground/Contents.swift
@@ -0,0 +1,17 @@
+
+var Ahmed : Dictionary <String,Int>
+Ahmed = [:]
+var Sulayman : Dictionary <String,Int>
+Sulayman = [:]
+
+Ahmed ["Madinah"] = 5
+Ahmed ["Taif"] = 6
+Ahmed ["Jeddah"] = 2
+Sulayman ["Madinah"] = 3
+Sulayman ["Riyadh"] = 2
+Sulayman ["Tabuk"] = 4
+Sulayman ["Taif"] = 7
+Sulayman ["khobar"] = 1
+Sulayman ["Abha"] = 2
+print("Ahmed\(Ahmed)")
+print("Sulayman\(Sulayman)")
---
+++ set Homework.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+var Ahmed : Set<String> = []
+var Sulayman : Set<String> = []
+Ahmed.insert("Madinah")
+Ahmed.insert("Taif")
+Ahmed.insert("Jeddah")
+Sulayman.insert("Madinah")
+Sulayman.insert("Riyadh")
+Sulayman.insert("Tabuk")
+Sulayman.insert("Taif")
+Sulayman.insert("Khobar")
+Sulayman.insert("Abha")
+print("They both visit\(Ahmed.intersection(Sulayman))")
+
+
