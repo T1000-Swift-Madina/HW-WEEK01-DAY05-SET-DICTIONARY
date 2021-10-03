8fe08de4a9c4bcb5b9cba65ce6abb88a3496802d
Mohammed Mabruk <Mohammed.Mabruk.med.s@tuwaiq.edu.sa>
2021-10-03 23:23:45 +0300

    add homework5

---
+++ (set).playground/Contents.swift
@@ -0,0 +1,14 @@
+
+var ahmed : Set = ["medina","qasim","dammam"]
+var sulaymman : Set = [ "medina","jeddah", "makkah","dammam","yanbu","jazan"]
+
+
+
+print (ahmed.union(sulaymman))
+print (ahmed.intersection(sulaymman))
+print (ahmed.symmetricDifference(sulaymman))
+print (ahmed.subtracting(sulaymman))
+
+
+
+
