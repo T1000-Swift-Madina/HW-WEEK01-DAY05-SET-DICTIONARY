2d4f024daa5428e3527cf86ee0175b729344bdc6
Mohammed Mabruk <Mohammed.Mabruk.med.s@tuwaiq.edu.sa>
2021-10-03 22:51:06 +0300

    add homework dic

---
+++ dic.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+var dict : [String:Int] = [:]
+var ahmed :[String:Int] = [ : ]
+
+var sulayman :[String:Int] = [ : ]
+
+
+ahmed ["medina"] = 2
+ahmed ["qasim"] = 2
+ahmed ["dammam"] = 3
+
+sulayman ["medina"] = 3
+
+sulayman ["makkah"] = 2
+sulayman ["dammam"] = 3
+sulayman ["jeddah"] = 1
+sulayman ["yanbu"] = 5
+sulayman ["jazan"] = 1
+
+
+
+
+print (ahmed)
+print (sulayman)
+
+
