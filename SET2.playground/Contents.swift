8953702ed4f215d8c2ed5186a2b09a31f56c3a98
reem alsubhi <Reem.Alsubi.med.s@tuwaiq.edu.sa>
2021-10-07 10:26:47 +0300

    HWset

---
+++ SET2.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var greeting = "Hello, playground"
+import UIKit
+
+
+
+
+var Ahamd:Set<String>  = ["madina","jeddah","maccah"]
+
+
+var Sulaman :Set = ["madina","riydah","maccah","abha","dammam","hail"]
+
+var city = Ahamd.intersection(Sulaman)
+
+   print(city)
+
+
+var DICAhmad :Dictionary<String,Int> = ["madina" : 4, "jeddah" : 6, "maccah" : 5 ]
+
+var DICSulaman  :Dictionary<String,Int> = ["madina" :2 ,"riydah" : 5 ,"maccah" : 6 ,"abha" :8 ,"dammam" : 3 ,"hail" : 9]
+
+
+print(DICAhmad)
+print(DICSulaman)
+
